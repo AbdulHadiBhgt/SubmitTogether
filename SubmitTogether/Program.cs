dbfea430e05fddfc1577df42a34f378295f9d9e2
Abdul Hadi Bhagat <80905227+AbdulHadiBhgt@users.noreply.github.com>
2022-01-06 13:07:49 +0500

    Add project files.

---
+++ SubmitTogether/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace SubmitTogether
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ids = new string[] { "1", "2", "3" };
+
+            for (int i = 0; i < ids.Length; i++)
+            {
+                thread t = new thread(ids[i]);
+            }
+        }
+    }
+
+    public class thread
+    {
+        public static int count = 0;
+        public string id;
+        public thread(string id)
+        {
+            this.id = id;
+            Thread t = new Thread(new ThreadStart(() =>
+            {
+                try
+                {
+                    if (this.id == "1")
+                    {
+                        Thread.Sleep(500);
+                        int i = 1;
+                        int j = 0;
+                        int k = i / j;
+                    }
+                    else if (this.id == "2")
+                    {
+                        Thread.Sleep(5000);
+                    }
+                    else if (this.id == "3")
+                    {
+                        Thread.Sleep(1000);
+                    }
+
+                    count++;
+
+                    while (count < 3)
+                    {
+                        Console.WriteLine("Waiting : {0}", this.id);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    count++;
+                }
+                finally
+                {
+                    Console.WriteLine("Finished : {0}", this.id);
+                }
+
+
+
+            }));
+            t.Start();
+        }
+    }
+
+}
